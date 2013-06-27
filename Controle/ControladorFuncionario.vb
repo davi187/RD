7935cbd170b29a403f2e842c01e4d5a0eec98ede
JhonyCosmo <jhonycosmo@gmail.com>
2013-06-26 23:26:54 -0300

    Comit apos a atualização com o de Davi

--- Controle/ControladorFuncionario.vb
+++ Controle/ControladorFuncionario.vb
@@ -2,7 +2,9 @@
     Public Shared Function getFuncionarios() As List(Of Funcionario)
 
         Dim listaFuncionarios As List(Of Funcionario)
-        Dim rs As DataSet = Driver.DataSet("Select * FROM Pessoas WHERE TipoCadastro=2 AND dataDesativado=''")
+
+        Dim rs As DataSet = Driver.Reader("Select * FROM Pessoas WHERE TipoCadastro=2 AND dataDesativado=''")
+
 
 
         For Each linha As DataRow In rs.Tables(0).Rows
@@ -16,16 +18,7 @@
             func.CVendedor = linha.Item("bairro")
             func.DataAlterado = linha.Item("bairro")
             func.DataCadastro = linha.Item("bairro")
-            'func.DataCriado
-            'func.DataDesativado
-            'func.Email
-            'func.Endereco
-            'func.Fantasia
-            'func.IERG
-            'func.PontoReferencia
-            'func.TipoCadastro
-            'func.TipoCadastro
-            'func.
+         
 
         Next
 
