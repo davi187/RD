79f80e02ea658052efb5a0d619de78357db8e9a6
JhonyCosmo <jhonycosmo@gmail.com>
2013-07-15 21:39:09 -0300

    Commit Correção

---
+++ Util/SqlFactory.vb
@@ -0,0 +1,14 @@
+﻿Public Class SqlFactory
+    Structure campo
+        Public campo As String
+        Public valor As String
+    End Structure
+    Private campos As List(Of campo)
+    Public Sub New()
+
+    End Sub
+    Public Sub addCampo(ByVal campo As campo)
+
+    End Sub
+
+End Class
