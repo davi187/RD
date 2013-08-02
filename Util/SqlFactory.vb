86deac58ce692783c57f8a321c54042f99783123
JhonyCosmo <jhonycosmo@gmail.com>
2013-08-01 22:54:34 -0300

    1

--- Util/SqlFactory.vb
+++ Util/SqlFactory.vb
@@ -3,12 +3,15 @@
         Public campo As String
         Public valor As String
     End Structure
-    Private campos As List(Of campo)
+    Private listaCampos As List(Of campo)
     Public Sub New()
-
+        listaCampos = New List(Of campo)
     End Sub
     Public Sub addCampo(ByVal campo As campo)
-
+        listaCampos.Add(campo)
     End Sub
+    Public Sub addCampo(ByVal campo As String, ByVal valor As String)
 
+        'listaCampos.Add(campo)
+    End Sub
 End Class
